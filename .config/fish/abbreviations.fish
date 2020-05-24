36c9967898f83ddf5bf383d118fadfe97484fb67
Edd <edd@vega.xyz>
2020-05-24 18:11:52 +0100

    Add fish abbreviations and aliases

---
+++ .config/fish/abbreviations.fish
@@ -0,0 +1,45 @@
+
+abbr --add dl 'cd ~/Downloads'
+abbr --add dt 'cd ~/Desktop'
+abbr --add g 'git'
+abbr --add y 'yadm'
+
+# git shortcuts
+abbr --add ga 'git add'
+abbr --add gaa 'git add .'
+abbr --add gb 'git branch'
+abbr --add gba 'git branch -a'
+abbr --add gbd 'git branch -D'
+abbr --add gbl 'git branch | grep "*" | sed "s/* //"'
+abbr --add gc 'git commit'
+abbr --add gca 'git commit -a'
+abbr --add gcd 'cd (git rev-parse --show-toplevel)'
+abbr --add gco 'git checkout'
+abbr --add gcob 'git checkout -b'
+abbr --add gcod 'git checkout development'
+abbr --add gcom 'git checkout master'
+abbr --add gcp 'git cherry-pick'
+abbr --add gcs 'git commit -sS'
+abbr --add gd 'git diff'
+abbr --add gfp 'git fetch --prune'
+abbr --add gi 'git ignore'
+abbr --add gl "git log --pretty='format:%C(Yellow)%h%Creset %C(Blue)%ar%Creset %an - %s' --graph"
+abbr --add gm 'git merge --no-ff'
+abbr --add gpoh 'git push origin HEAD'
+abbr --add gpull 'git pull'
+abbr --add gpush 'git push'
+abbr --add grh 'git reset --hard'
+abbr --add gst 'git status'
+abbr --add gt 'git tag'
+abbr --add gts 'git tag -s'
+abbr --add lg 'lazygit'
+
+
+# yadm shortcuts
+abbr --add ya 'yadm add'
+abbr --add yc 'yadm commit'
+abbr --add yd 'yadm diff'
+abbr --add yst 'yadm status'
+
+# Sources
+# https://github.com/herrbischoff/dotpr0n/blob/master/.config/fish/abbreviations.fish
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,24 @@
 starship init fish | source
+set fish_greeting ""
+
+alias l "ls -lah"
+alias vi "vim"
+alias nvim "vim"
+
+set -gx EDITOR vim
+set -gx VISUAL vim
+
+if test -d $HOME/go
+  set GOPATH $HOME/go
+  set PATH $PATH $GOPATH/bin
+end
+
+# fzf
+if test -d $HOME/.fzf/shell
+  source $HOME/.fzf/shell/key-bindings.fish
+  set -x FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow --glob "!.git/*"'
+end
+
 set -l foreground f8f8f2
 set -l selection  44475a
 set -l comment    6272a4
@@ -28,3 +48,5 @@ set -g fish_color_selection      --background=$selection
 set -g FZF_DEFAULT_COMMAND 'ag --nocolor --ignore node_modules -g ""'
 set -g FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 
+source $HOME/.config/fish/abbreviations.fish > /dev/null 2>&1
+set -gx PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin $PATH
